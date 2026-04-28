e51338cd526d8c81b1f081ef8e4e9576524d8ac2
hk2022912 <butlay.hazelkate@gmail.com>
2026-04-28 22:19:16 +0800

    STATUS UPDATE: BUILD.SH FILE AND SETTINGS.PY CONFIG TO RENDER

--- firs_backend/firs_backend/settings.py
+++ firs_backend/firs_backend/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
-    )
+    'default': dj_database_url.config{
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Quick-start development settings - unsuitable for production
