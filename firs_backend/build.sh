99482c91d0a63d3747c24aab1c64122ffd658c1e
hk2022912 <butlay.hazelkate@gmail.com>
2026-04-29 14:52:58 +0800

    fix create_superuser path in build.sh

--- firs_backend/build.sh
+++ firs_backend/build.sh
@@ -4,13 +4,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py shell << 'EOF'
-from django.contrib.auth.models import User
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'operation.firs.2026@gmail.com', 'Firs2026!')
-    print('Superuser created')
-else:
-    print('Superuser already exists')
-EOF
-
-python create_superuser.py
+python firs_backend/create_superuser.py
