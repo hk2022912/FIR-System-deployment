3e0113794fb2ffb1c85f4a7e92645355005eae57
hk2022912 <butlay.hazelkate@gmail.com>
2026-04-29 15:19:54 +0800

    use inline shell command to create superuser

--- firs_backend/build.sh
+++ firs_backend/build.sh
@@ -4,4 +4,11 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python create_superuser.py
+python manage.py shell -c "
+from django.contrib.auth.models import User
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'operation.firs.2026@gmail.com', 'Firs2026!')
+    print('Superuser created')
+else:
+    print('Superuser already exists')
+"
