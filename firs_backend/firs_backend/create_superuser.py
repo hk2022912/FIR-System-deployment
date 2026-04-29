a690f3eb01dfd7b6814a8cf8222e304f0b093a17
hk2022912 <butlay.hazelkate@gmail.com>
2026-04-29 14:50:12 +0800

    use separate script to create superuser

--- firs_backend/firs_backend/create_superuser.py
+++ firs_backend/firs_backend/create_superuser.py
@@ -1,16 +1,14 @@
-#!/usr/bin/env bash
-set -o errexit
+import os
+import django
 
-pip install -r requirements.txt
-python manage.py collectstatic --no-input
-python manage.py migrate
-python manage.py shell << 'EOF'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'firs_backend.settings')
+django.setup()
+
 from django.contrib.auth.models import User
+
 if not User.objects.filter(username='admin').exists():
     User.objects.create_superuser('admin', 'operation.firs.2026@gmail.com', 'Firs2026!')
     print('Superuser created')
 else:
     print('Superuser already exists')
-EOF
-
-python create_superuser.py
+    
